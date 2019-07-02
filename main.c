4042d22a83921f43260d69b9157af8cf12ac65fc
Ali Yazdanpanah <34602222+Ali-Yazdanpanah@users.noreply.github.com>
2019-07-02 17:25:31 +0430

    Update main.c

--- main.c
+++ main.c
@@ -8,31 +8,6 @@
 
 int **A_Matrix, **D_Matrix;
 
-int main(int argc, char **argv){
-	int N;     
-	N = atoi(argv[1]);  //Read the console inputs
-	int n = pow(2, N);
-	
-    int i, j, k;
-
-	D_Matrix = malloc(N * sizeof(int *));
-    for (i = 0; i < N; i++)
-    {
-        D_Matrix[i] = malloc(N * sizeof(int));
-    }
-    makeAdjacency(N);
-    clock_t start = clock();  //First time measurement
-    // algorithm -->
-    floyd_warshall_serial(A_Matrix,D_Matrix,N);
-    // <--
-	clock_t end = clock();   //Final time calculation and convert it into seconds
-	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-	printf("Elapsed time = %f sec\n", seconds);
-
-    
-}
-
-
 void makeAdjacency(int n){   //Set initial values to node distances
     int N=n;
     int i,j;
@@ -60,3 +35,25 @@ void makeAdjacency(int n){   //Set initial values to node distances
         }
     }
 }
+
+int main(int argc, char **argv){
+    int N;     
+	N = atoi(argv[1]);  //Read the console inputs
+    int i, j, k;
+	D_Matrix = malloc(N * sizeof(int *));
+    for (i = 0; i < N; i++)
+    {
+        D_Matrix[i] = malloc(N * sizeof(int));
+    }
+    makeAdjacency(N);
+    clock_t start = clock();  //First time measurement
+    // algorithm -->
+    floyd_warshall_serial(A_Matrix,D_Matrix,N);
+    // <--
+	clock_t end = clock();   //Final time calculation and convert it into seconds
+	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("Elapsed time = %f sec\n", seconds);
+}
+
+
+
